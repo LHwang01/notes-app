c4ac6c60b3951a2871faa0a71cb122d82ac4a23f
LHwang01 <LawrenceHwang01@gmail.com>
2021-10-10 14:52:00 -0400

    Finished AddNote Feature

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -4,12 +4,13 @@ import AddNote from "./AddNote"
 const NotesList = ({ notes, handleAddNote }) => {
     return(
         <div className="notes-list">
-            {notes.map((note) => 
-            <Note
-                id={note.id} 
-                text={note.text} 
-                date={note.date} 
-            />)}
+            {notes.map((note) => (
+                <Note
+                    id={note.id} 
+                    text={note.text} 
+                    date={note.date} 
+                />
+            ))}
             <AddNote handleAddNote={handleAddNote} />
         </div>
     )
